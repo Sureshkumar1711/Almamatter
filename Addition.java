d2f65b0b86ecaf5d799bf81cb34c39d9ba98aac9
Sureshkumar1711 <87181382+Sureshkumar1711@users.noreply.github.com>
2021-07-09 16:07:56 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,19 @@
+package javaday5;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter two numbers");
+
+int a=100;
+int b=200;
+System.out.println(a + " " + b);
+
+int sum= a+b ;
+
+System.out.println("the sum is: " + sum);
+
+	}
+
+}
