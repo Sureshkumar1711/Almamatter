f9d5a0d23c5d64bf85088fe92eb265bce784960a
Sureshkumar1711 <87181382+Sureshkumar1711@users.noreply.github.com>
2021-07-10 02:50:40 +0530

    Add files via upload

---
+++ Swaptwonumbers.java
@@ -0,0 +1,25 @@
+package javaday5;
+
+public class Swaptwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		float first = 3.50f, second = 6.50f;
+		
+		System.out.println("--Before swap--");
+		System.out.println("First number = " + first);
+		System.out.println("Second number = " + second);
+		
+		float temporary = first;
+		
+		first = second;
+		
+		second = temporary;
+		
+		System.out.println("--After swap--");
+		System.out.println("First number = " + first);
+		System.out.println("Second number = " + second);
+	}
+
+}
